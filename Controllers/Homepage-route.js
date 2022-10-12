468e6b167f1c2692d268e91da0a4d53c2dc89811
Daniel Romero <you@example.com>
2022-10-11 19:19:59 -0700

    working server

--- Controllers/Homepage-route.js
+++ Controllers/Homepage-route.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-const path = require('path');
 
 // This is the 'get' route 
 router.get('/', async (req, res) => {
-  res.render('all');
+  res.render('homepage');
 });
 
+
+
+module.exports = router;
