98d6029d778f41b886304045999d539a93fbeb2d
malek mekdashi <malekmekdashi@gmail.com>
2022-10-11 21:19:19 -0700

    worked on login post/get routes along with models

--- Controllers/loginpage-route.js
+++ Controllers/loginpage-route.js
@@ -1,8 +1,33 @@
+const User = require('../models/User');
+
 const router = require('express').Router();
 
+const withAuth = require('../utils/auth');
+
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: }],
+    });
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // This is the 'get' route 
-router.get('/', async (req, res) => {
-  res.render('homepage');
+router.get('/login', async (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+  res.render('login');
 });
 
 
