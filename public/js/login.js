80621bfc466f64eef11fc121037a700b90f74bda
Daniel Romero <you@example.com>
2022-10-15 16:44:38 -0700

    routes

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ const loginFormHandler = async (event) => {
     const password = document.querySelector('#password-login').value.trim();
 
     if (email && password) {
-        const response = await fetch('/api/users/login', {
+        const response = await fetch("", {
             method: 'POST',
             body: JSON.stringify({ email, password }),
             headers: { 'Content-Type': 'application/json' },
@@ -27,7 +27,7 @@ const signupFormHandler = async (event) => {
     const password = document.querySelector('#password-signup').value.trim();
 
     if (name && email && password) {
-        const response = await fetch('/api/users', {
+        const response = await fetch('/Controllers/api/userRoutes.js', {
             method: 'POST',
             body: JSON.stringify({ name, email, password }),
             headers: { 'Content-Type': 'application/json' },
--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 // Dependencies
 const express = require('express');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
 const path = require('path');
 const hbs = exphbs.create({});
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require('./config/connection');
+
+
 
 // Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//  Creats a seassion 
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    // life of cookies 
+    maxAge:24*60*60*1000,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+// Uses Session
+app.use(session(sess));
 
 
 // Set Handlebars as the default template engine.
