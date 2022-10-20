200b8dfd44eb44d044cb4a3475fa71a17a93db72
Daniel Romero <you@example.com>
2022-10-19 23:14:47 -0700

    mirror fixes

--- Controllers/api/index.js
+++ Controllers/api/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 // Nicole - NTS you added the loginpage-Routes
-const loginRoutes = require('./loginpage-Route');
-const userRoutes = require('./userRoutes');
+const userRoutes = require("./userRoutes");
 
 router.use('/users', userRoutes);
 
