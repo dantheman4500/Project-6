d50bbcbd30d78714d7a9daf6e0b92e42b0c5c531
Daniel Romero <you@example.com>
2022-10-20 16:00:21 -0700

    fixxed this bitch

--- Controllers/Homepage-route.js
+++ Controllers/Homepage-route.js
@@ -1,12 +1,62 @@
 const router = require('express').Router();
+const { Reviews, User } = require('../models');
+const withAuth = require('../utils/auth');
 
 // This is the home route 
 router.get('/', async (req, res) => {
-    res.render('homepage');
+  try {
+// Get all Reviews and JOIN with user data
+    const rewiewData = await Reviews.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const profile = profileData.map((reviewData) => reviewData.get({ plain: true }));
+
+// Pass serialized data and session flag into template
+    res.render('homepage', {
+      profile,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+// Use withAuth middleware to prevent access to route
+  router.get('/profile', withAuth, async (req, res) => {
+    try {
+// Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Reviews }],
+      });
+
+      const user = userData.get({ plain: true });
+
+      res.render('profile', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
   });
 
-router.get('/login',async(req,res)=>{
-  res.render('login');
-});
-  
-module.exports = router;
+  router.get('/login', (req, res) => {
+// If the user is already logged in go to profile
+    if (req.session.logged_in) {
+      res.redirect('/profile');
+      return;
+    }
+    res.render('login');
+  });
+
+  module.exports = router;
--- seeds/seeds.js
+++ seeds/seeds.js
@@ -3,7 +3,7 @@ const sequelize = require('../config/connection');
 const { User, Review} = require('../models');
 
 const userData = require('./userData.json');
-const reviewData = require('./reviewData.json')
+// const reviewData = require('./reviewData.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -13,16 +13,18 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const review of reviewData) {
-    await Review.create({
-      ...review,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
+  // for (const review of reviewData) {
+  //   await Review.create({
+  //     ...review,
+  //     user_id: users[Math.floor(Math.random() * users.length)].id,
+  //   });
+  // }
 
   
 
   process.exit(0);
 };
+
+
 
 seedDatabase();
